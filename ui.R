1eb09728c3bd27b60b64bd943ee1ad08c3273436
Siqi Liu <flycat1989@gmail.com>
2015-04-03 16:03:25 -0400

    added instructions

--- ui.R
+++ ui.R
@@ -31,12 +31,21 @@ shinyUI(fluidPage(
         textInput("endoUse","Use Endogenous Control:","Actin"),
         actionButton("plotbutton", "Confirm parameters and plot")
         ),
-      tabPanel("Download data",
+      tabPanel("Download Data",
         downloadButton('downloadnumerical', 'Download Normalized Data')
         )
     )),
     mainPanel(
       tabsetPanel(
+        tabPanel("Instructions",
+                 h4('Please read the instructions carefully before starting the analysis'),
+                 p('1. This program can accept txt files or csv files with different types of seperations, please specify the seperator from the File Upload Info tab.'),
+                 p('2. The data file should have at least 3 columns, with the column Sample,Target,Ct. Please prepare the data file before uploading.'),
+                 p('3. Once the data file is successfully uploaded, you can go to the Setting up Parameters tab. If multiple endogenous control genes are used in the experiment, you can type them into the Total Endogenous Controls filed, seperated by comma. If more than 3 endogenous controls are used, the M values for the controls will be calculated and show up in the Numeric Results section.'),
+                 p('4. You can based on the calculated M values to pick the desired endogenous controls for data normalization. Type the desired endogenous control genes in the Use Endogenous Control filed, separated by comma. The normalized gene expression will show up in the Numeric Result section.'),
+                 p('5. Once the parameters are setup, you can click Confirm Parameters and Plot button to plot the result graphs. The graphs can be visualized in the Graphic Results section'),
+                 p('6. You can download the normalized data from the Download Data tab. By clicking the Download Normalized Data, you can download the results after normalization for further processes')
+        ),
         tabPanel("Uploaded Data",
                  tableOutput('contents')
         ),
